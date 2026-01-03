1813f0a75f2cdc7bc39a84239898ddf91eb1d157
AKJ <avneeshjha1506@gmail.com>
2026-01-03 21:25:28 +0530

    update 1.0

--- index.js
+++ index.js
@@ -48,4 +48,5 @@ app.get("/", (req, res) => {
   return res.sendFile("/public/index.html");
 });
 
-server.listen(9000, () => console.log(`Server Started at PORT:9000`));
+const PORT = process.env.PORT || 9000;
+server.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
